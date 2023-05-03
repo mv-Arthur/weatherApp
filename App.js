cd70b6f31dae034cad0dabd76845b109cd58a13f
Arthur <malakhov.artur@inbox.ru>
2023-05-03 22:44:59 +0300

    f

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import axios from "axios";
+import { Select } from "native-base";
+import { StyleSheet } from "react-native";
 import {
   Text,
   Link,
@@ -11,7 +14,12 @@ import {
   extendTheme,
   VStack,
   Box,
+  Button,
+  Skeleton,
+  Modal,
+  FormControl,
 } from "native-base";
+import { View, Pressable, Image } from "native-base";
 import NativeBaseIcon from "./components/NativeBaseIcon";
 import { Platform } from "react-native";
 
@@ -25,6 +33,92 @@ const config = {
 export const theme = extendTheme({ config });
 
 export default function App() {
+  const [city, setCity] = React.useState("Moscow");
+  const [cityData, setCityData] = React.useState();
+  const [showModal, setShowModal] = React.useState(false);
+  const apiKey = "15b726b915a14cb2b62150855231704";
+  const [isLoading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res.data);
+        setCityData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setShowModal(true);
+        setLoading(true);
+      });
+  }, []);
+
+  React.useEffect(() => {
+    let url = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+    axios
+      .get(url)
+      .then((res) => {
+        console.log(res.data);
+        setCityData(res.data);
+        setLoading(false);
+      })
+      .catch((err) => {
+        setShowModal(true);
+        setLoading(true);
+      });
+  }, [city]);
+
+  const onChangeCity = (itemValue) => {
+    setCity(itemValue);
+  };
+
+  const styles = StyleSheet.create({
+    container: {
+      backgroundColor: "#0891b2",
+      paddingVertical: 16,
+      paddingHorizontal: 12,
+      borderRadius: 5,
+      alignSelf: "center",
+      width: 375,
+      maxWidth: "100%",
+    },
+    timings: {
+      color: "#fff",
+      fontSize: 14,
+    },
+    metaContainer: {
+      justifyContent: "space-between",
+    },
+    topContainer: {
+      flexDirection: "row",
+      justifyContent: "space-between",
+    },
+    avatar: {
+      height: 100,
+      width: 100,
+      borderRadius: 100,
+    },
+    description: {
+      color: "white",
+      marginTop: 5,
+      fontSize: 20,
+    },
+    button: {
+      backgroundColor: "#22d3ee",
+      alignSelf: "flex-start",
+      paddingHorizontal: 12,
+      paddingVertical: 4,
+      borderRadius: 2,
+    },
+    buttonText: {
+      fontWeight: "bold",
+      color: "white",
+      textTransform: "uppercase",
+      fontSize: 14,
+    },
+  });
+
   return (
     <NativeBaseProvider>
       <Center
@@ -33,32 +127,98 @@ export default function App() {
         px={4}
         flex={1}
       >
+        <Modal isOpen={showModal} onClose={() => setShowModal(false)}>
+          <Modal.Content maxWidth="400px">
+            <Modal.CloseButton />
+            <Modal.Header>Contact Us</Modal.Header>
+            <Modal.Body>Cannot connect to server, come back later</Modal.Body>
+            <Modal.Footer>
+              <Button.Group space={2}>
+                <Button
+                  variant="ghost"
+                  colorScheme="blueGray"
+                  onPress={() => {
+                    setShowModal(false);
+                  }}
+                >
+                  Cancel
+                </Button>
+              </Button.Group>
+            </Modal.Footer>
+          </Modal.Content>
+        </Modal>
+        {isLoading ? (
+          <VStack
+            w="90%"
+            maxW="400"
+            borderWidth="1"
+            space={8}
+            overflow="hidden"
+            rounded="md"
+            _dark={{
+              borderColor: "coolGray.500",
+            }}
+            _light={{
+              borderColor: "coolGray.200",
+            }}
+          >
+            <Skeleton h="40" />
+            <Skeleton.Text px="4" />
+            <Skeleton px="4" my="4" rounded="md" startColor="primary.100" />
+          </VStack>
+        ) : (
+          <View style={styles.container}>
+            <View style={styles.topContainer}>
+              <View style={styles.metaContainer}>
+                <View>
+                  <Text style={styles.timings}>
+                    local time and date: {cityData.location.localtime}
+                  </Text>
+                  <Text style={styles.description}>
+                    {cityData.location.country}
+                    {"\n"}
+                    {cityData.location.name}
+                  </Text>
+                </View>
+
+                <Text style={styles.buttonText}>
+                  Temperature in celsius {cityData.current.temp_c}°
+                </Text>
+                <Text style={styles.buttonText}>
+                  Temperature in fahrenheit {cityData.current.temp_f}°
+                </Text>
+              </View>
+              <Image
+                source={{
+                  uri: "http:" + cityData.current.condition.icon,
+                }}
+                style={styles.avatar}
+              />
+            </View>
+          </View>
+        )}
+
+        <Box maxW="300">
+          <Select
+            selectedValue={city}
+            minWidth="200"
+            accessibilityLabel="Choose city"
+            placeholder="Choose City"
+            _selectedItem={{
+              bg: "teal.600",
+            }}
+            mt={1}
+            onValueChange={onChangeCity}
+          >
+            <Select.Item label="New York" value="New York" />
+            <Select.Item label="Moscow" value="Moscow" />
+            <Select.Item label="Kazan" value="Kazan" />
+            <Select.Item label="Oslo" value="Oslo" />
+            <Select.Item label="Sydney" value="Sydney" />
+          </Select>
+          <Button onPress={() => console.log("hello world")}>Click Me</Button>
+        </Box>
         <VStack space={5} alignItems="center">
-          <NativeBaseIcon />
-          <Heading size="lg">Welcome to NativeBase</Heading>
-          <HStack space={2} alignItems="center">
-            <Text>Edit</Text>
-            <Box
-              _web={{
-                _text: {
-                  fontFamily: "monospace",
-                  fontSize: "sm",
-                },
-              }}
-              px={2}
-              py={1}
-              _dark={{ bg: "blueGray.800" }}
-              _light={{ bg: "blueGray.200" }}
-            >
-              App.js
-            </Box>
-            <Text>and save to reload.</Text>
-          </HStack>
-          <Link href="https://docs.nativebase.io" isExternal>
-            <Text color="primary.500" underline fontSize={"xl"}>
-              Learn NativeBase
-            </Text>
-          </Link>
           <ToggleDarkMode />
         </VStack>
       </Center>
